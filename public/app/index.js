1be5d207f011271ba39b2cb0faca2025d92e036c
lagen2011 <vitalij-kovalevich@mail.ru>
2018-07-12 09:02:51 +0300

    fixed src

--- public/app/index.js
+++ public/app/index.js
@@ -1,5 +1,11 @@
 import App from '/public/app/components/App.js';
 
+var headers = new Headers();
+// *** I set the header in order to solve the error above:
+// *** The value is set to "/" because this js is included in html file in upper folder.
+// *** I tried even "../" and many more others values...
+headers.append('Service-Worker-Allowed', '/');
+console.log(headers.get('Service-Worker-Allowed'));
 
 if ( 'serviceWorker' in navigator ) {
     navigator.serviceWorker.register('/public/sw.js', {scope: './'})
